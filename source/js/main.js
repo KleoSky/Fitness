efa89013f11f687d0ae99c199319eb5f1a4e692e
Вероника Неброева <v-kleo@yandex.ru>
2025-03-24 22:00:15 +0700

    Удаляет закомментированный текст

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,5 @@
 // https://swiperjs.com/get-started#installation
 
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
 import { switchTab } from './modules/price';
 switchTab();
 
